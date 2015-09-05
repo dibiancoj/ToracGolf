17e52e413a61fa034de3c5ee6d2ded0fc97d07ab
dibiancoj <DiBiancoj@gmail.com>
2015-09-05 16:35:14 -0400

    start of login

---
+++ src/ToracGolf/Controllers/SecurityController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ToracGolf.Controllers
+{
+    public class SecurityController : Controller
+    {
+        
+        [Route("LogIn")]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+    }
+}
