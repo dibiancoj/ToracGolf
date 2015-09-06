2abe54d35da7b4220e1aff9fb65729063ed6b07f
dibiancoj <DiBiancoj@gmail.com>
2015-09-05 21:06:00 -0400

    log in page starting to look really nice!

--- src/ToracGolf/Controllers/SecurityController.cs
+++ src/ToracGolf/Controllers/SecurityController.cs
@@ -5,22 +5,83 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using ToracGolf.ViewModels.Navigation;
 using ToracGolf.ViewModels.Security;
+using Microsoft.AspNet.Authorization;
+using ToracGolf.Models;
+using Microsoft.AspNet.Identity;
 
 namespace ToracGolf.Controllers
 {
+
+    [Authorize]
     public class SecurityController : Controller
     {
 
+        #region Constructor
+
+        public SecurityController(SignInManager<ApplicationUser> signInManagerAPI)
+        {
+            SignInManagerAPI = signInManagerAPI;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private SignInManager<ApplicationUser> SignInManagerAPI { get; }
+
+        #endregion
+
+        #region Methods
+
+        [HttpGet]
+        [AllowAnonymous]
         [Route("LogIn")]
         public IActionResult LogIn()
         {
             var breadCrumb = new List<BreadcrumbNavItem>();
 
             breadCrumb.Add(new BreadcrumbNavItem("Home", "#"));
-            breadCrumb.Add(new BreadcrumbNavItem("LogIn", "#"));
+            breadCrumb.Add(new BreadcrumbNavItem("Sign In", "#"));
 
             return View(new LogInViewModel(breadCrumb));
         }
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
+        //{
+        //    ViewData["ReturnUrl"] = returnUrl;
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        // This doesn't count login failures towards account lockout
+        //        // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+        //        var result = await SignInManagerAPI.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToLocal(returnUrl);
+        //        }
+        //        if (result.RequiresTwoFactor)
+        //        {
+        //            return RedirectToAction(nameof(SendCode), new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+        //        }
+        //        if (result.IsLockedOut)
+        //        {
+        //            return View("Lockout");
+        //        }
+        //        else
+        //        {
+        //            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        //            return View(model);
+        //        }
+        //    }
+
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+        #endregion
 
     }
 }
