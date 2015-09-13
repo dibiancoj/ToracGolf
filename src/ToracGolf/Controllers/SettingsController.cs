377b95f90c7b9f52a87cb640fecc5ac823bef99d
dibiancoj <DiBiancoj@gmail.com>
2015-09-13 14:52:20 -0400

    my settings. template name to use

---
+++ src/ToracGolf/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+
+namespace ToracGolf.Controllers
+{
+
+    [Authorize]
+    public class SettingsController : Controller
+    {
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ChangeMySettings", Name = "ChangeMySettings")]      
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
