424e6ddb365607f3929d8bfa50ac554792b0fcd0
dibiancoj <DiBiancoj@gmail.com>
2015-10-04 21:13:18 -0400

    working numeric only.

--- src/ToracGolf/wwwroot/js/Angular/Common/NumberOnlyDirective.js
+++ src/ToracGolf/wwwroot/js/Angular/Common/NumberOnlyDirective.js
@@ -8,12 +8,10 @@
         return {
             require: 'ngModel', //this way we can push stuff to the model
             restrict: 'A',
+            scope: true,
             link: function (scope, element, attrs, modelCtrl) {
 
-                scope.allowInts = attrs.onlyint;
-
-                //when we push a value
-                modelCtrl.$parsers.push(function (inputValue) {
+                function IsValidNumber(inputValue, onlyAllowInts) {
 
                     // this next if is necessary for when using ng-required on your input. 
                     // In such cases, when a letter is typed first, this parser will be called
@@ -22,10 +20,16 @@
                         return ''
                     }
 
-                    NEED TO FIGURE OUT HOW TO GRAB THE ATTRIBUTE numbers-only-allowOnlyInts and use it below to remove the '.' in teh regex expression
-
                     //this will strip out anything that we don't accept. so if user has 0t...it will remove the t
-                    var transformedInput = inputValue.replace(/[^0-9.]/g, '');
+                    var transformedInput;
+
+                    //do they only want int's?
+                    if (onlyAllowInts) {
+                        transformedInput = inputValue.replace(/[^0-9]/g, '');
+                    }
+                    else {
+                        transformedInput = inputValue.replace(/[^0-9.]/g, '');
+                    }
 
                     //make sure it's a number
                     if (isNaN(transformedInput)) {
@@ -46,9 +50,21 @@
 
                     //return the transformed input value
                     return transformedInput;
+                }
+
+                //set the scope variable if we want only ints
+                scope.allowInts = attrs.numbersOnlyAllowonlyints.toLowerCase() === 'true';
+
+                //add the new parser
+                modelCtrl.$parsers.push(function (inputValue) {
+
+                    //grab the only allow ints
+                    var onlyAllowInts = scope.allowInts;
+
+                    //go set the validity
+                    return IsValidNumber(inputValue, onlyAllowInts);
                 });
             },
-
         }
 
     })
