477b3e6b0b4383d4d4e885931410da921142efed
dibiancoj <DiBiancoj@gmail.com>
2015-11-16 21:35:45 -0500

    working react example

--- src/ToracGolf/wwwroot/js/React/ChangePassword/ChangeMyPassword.js
+++ src/ToracGolf/wwwroot/js/React/ChangePassword/ChangeMyPassword.js
@@ -1,1 +1,18 @@
-﻿
+﻿var validationApplication = React.createClass({
+    render: function () {
+
+         return React.DOM.li(null, this.props.ErrorMessage);
+    }
+});
+
+var validateFactory = React.createFactory(validationApplication);
+
+
+
+
+$(document).ready(function () {
+    $('#ChangePassword').click(function () {
+        //go save something
+        ReactDOM.render(validateFactory({ ErrorMessage: 'Invalid Jason' }), document.getElementById('ValidationError'));
+    });
+});
