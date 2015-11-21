eb2f3462563b1a0679e3d2bb9f8bb45ada9d3651
dibiancoj <DiBiancoj@gmail.com>
2015-11-21 11:11:56 -0500

    add password input to change password

--- src/ToracGolf/wwwroot/js/React/ChangePassword/ChangeMyPassword.jsx
+++ src/ToracGolf/wwwroot/js/React/ChangePassword/ChangeMyPassword.jsx
@@ -1,90 +1,92 @@
 ﻿//gets called in the form using <ErrorList ErrorItems={this.state.Errors} />
 //this is the error validation <ul>
 var ErrorList = React.createClass({
-    render: function() {
+    render: function () {
 
-        var createItem = function(itemText, index) {
-            return <li key={index}>{itemText}</li>;
+        var createItem = function (itemText, index) {
+            return <li key={index }>{itemText}</li>;
         };
 
         //props.ErrorItems comes from the attribute when we specify <ErrorList ErrorItems
         return <ul>{this.props.ErrorItems.map(createItem)}</ul>;
     }
-}); 
+});
+
+var PasswordInput = React.createClass({
+    render: function () {
+        return <input id={this.props.IdToUse} onChange={this.props.onChangeEvent} type="password" maxLength="100"></input>
+    }
+});
 
 var ChangePasswordForm = React.createClass({
-    getInitialState: function() {
-        return { Errors:[] }
+    getInitialState: function () {
+        return { Errors: [] }
     },
-    handleSubmit: function() {
+    handleSubmit: function () {
 
         //go run validation with submit clicked
         var result = this.validation(true);
 
         //go make an ajax call if we have 0 errors
-        if (result.length == 0)
-        {
+        if (result.length == 0) {
             //go make ajax call here
-            this.setState({Errors: ['controller error on mvc side after ajax call']});
-        } 
+            this.setState({ Errors: ['controller error on mvc side after ajax call'] });
+        }
     },
-    handleChange: function(field, e) {
+    handleChange: function (e) {
 
         //something has changed, go run the form validation
         this.validation(false);
     },
-    getFormData: function() {
-        return { 
+    getFormData: function () {
+        return {
             CurrentPW: $('#CurrentPassword').val(),
             NewPw1: $('#NewPassword1').val(),
             NewPw2: $('#NewPassword2').val(),
         };
     },
-    validation: function(onFormSubmit){
+    validation: function (onFormSubmit) {
         var errors = [];
 
         //grab the form data
         var formData = this.getFormData();
 
-        if (onFormSubmit)
-        {
-            if (formData.CurrentPW.length == 0 || formData.NewPw1.length == 0 || formData.NewPw2.length == 0)
-            {
+        if (onFormSubmit) {
+            if (formData.CurrentPW.length == 0 || formData.NewPw1.length == 0 || formData.NewPw2.length == 0) {
                 errors.push("Please Fill In All The Fields.");
             }
         }
 
-        if ((formData.NewPw1.length > 0 || formData.NewPw2.length > 0) && (formData.NewPw1 != formData.NewPw2))
-        {
+        if ((formData.NewPw1.length > 0 || formData.NewPw2.length > 0) && (formData.NewPw1 != formData.NewPw2)) {
             errors.push("New Password Fields Don't Match.");
         }
-        
-        this.setState({Errors: errors});
+
+        this.setState({ Errors: errors });
 
         return errors;
     },
-    render: function() {
-        return (            
+    render: function () {
+        return (
                         <fieldset>
                                     <section>
                                         <label className="label">Current Password</label>
                                         <label className="input">
                                             <i className="icon-append fa fa-edit"></i>
-                                            <input id="CurrentPassword" type="password" onChange={this.handleChange.bind(this, 'CurrentPW')} maxLength="100"></input>
+                                           <PasswordInput IdToUse="CurrentPassword" onChangeEvent={this.handleChange.bind(this)}></PasswordInput>
                                         </label>
                                     </section>
                                     <section>
                                         <label className="label">New Password</label>
                                         <label className="input">
                                             <i className="icon-append fa fa-edit"></i>
-                                            <input id="NewPassword1" type="password" onChange={this.handleChange.bind(this, 'NewPw1')} maxLength="100"></input>
+                                             <PasswordInput IdToUse="NewPassword1" onChangeEvent={this.handleChange.bind(this)}></PasswordInput>
                                         </label>
                                     </section>
                                     <section>
                                         <label className="label">Retype New Password</label>
                                         <label className="input">
                                             <i className="icon-append fa fa-edit"></i>
-                                            <input id="NewPassword2" type="password" onChange={this.handleChange.bind(this, 'NewPw2')} maxLength="100"></input>
+                                            <PasswordInput IdToUse="NewPassword2" onChangeEvent={this.handleChange.bind(this)}></PasswordInput>
                                         </label>
                                     </section>
                                     <section>
@@ -111,4 +113,4 @@ $(document).ready(function () {
           <ChangePasswordForm />,
           document.getElementById('ChangePwForm'));
 
-});
+})
