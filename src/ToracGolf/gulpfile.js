7776f4b0207ef500acc295ab8cfa7b0432e6fade
dibiancoj <DiBiancoj@gmail.com>
2015-12-13 15:22:56 -0500

    remove site.js (site.min.js) - not used

--- src/ToracGolf/gulpfile.js
+++ src/ToracGolf/gulpfile.js
@@ -16,7 +16,7 @@ paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
+//paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
 //react
@@ -32,22 +32,23 @@ paths.minReactFormatting = paths.webroot + "js/**/ReactFormatting.js";
 paths.reactCourseStats = paths.webroot + 'js/**/CourseStats.jsx';
 paths.minReactCourseStats = paths.webroot + "js/**/CourseStats.js";
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
-});
+//gulp.task("clean:js", function (cb) {
+//    rimraf(paths.concatJsDest, cb);
+//});
 
 gulp.task("clean:css", function (cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+//gulp.task("clean", ["clean:js", "clean:css"]);
+gulp.task("clean", ["clean:css"]);
 
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
+//gulp.task("min:js", function () {
+//    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+//        .pipe(concat(paths.concatJsDest))
+//        .pipe(uglify())
+//        .pipe(gulp.dest("."));
+//});
 
 gulp.task("min:css", function () {
     return gulp.src([paths.css, "!" + paths.minCss])
@@ -147,7 +148,8 @@ gulp.task("BoomerangResponsiveMobile:min:js", function () {
         .pipe(gulp.dest("."));
 })
 
-gulp.task("min", ["min:js", "min:css"]);
+//gulp.task("min", ["min:js", "min:css"]);
+gulp.task("min", ["min:css"]);
 
 
 //in project.json i can say on publish to server, run this task
