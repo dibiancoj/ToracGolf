71a81412ddb549134b2f043c1827f6334a107bd9
Jason <dibiancoj@gmail.com>
2015-12-17 21:39:34 -0500

    more angular 2

--- src/ToracGolf/wwwroot/js/Angular2/NewsFeed/NewsFeed.js
+++ src/ToracGolf/wwwroot/js/Angular2/NewsFeed/NewsFeed.js
@@ -1,10 +1,27 @@
 ﻿(function (app) {
     app.AppComponent = ng.core
       .Component({
+          directives: [FORM_DIRECTIVES],
           selector: 'my-app',
-          template: '<h1>My First Angular 2 App</h1>'
+          template: '<h1>My First Angular 2 App {{bla}}</h1>' +
+                    '<input id="txt" type="text" />' +
+                    '<button (click)="testClick()">button text</button>' +
+                    '<span>{{test}}</span>' +
+
+                    '<br /><br />' + 
+                    '<input [(ng-model)]="todoModel"/> + <span>{{todoModel}}'
+
       })
       .Class({
-          constructor: function () { }
+          constructor: function () {
+              this.bla = 'Jason';
+              this.test = '';
+              this.todoModel = 'test';
+          },
+
+          testClick: function () {
+              this.test = $('#txt').val();
+              //alert(this.test);
+          }
       });
 })(window.app || (window.app = {}));
