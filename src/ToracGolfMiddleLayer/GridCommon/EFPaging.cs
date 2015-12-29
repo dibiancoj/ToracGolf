cb5b64c6137fe633d734169177958249ec00e212
dibiancoj <dibiancoj@gmail.com>
2015-12-29 14:37:41 -0500

    move ef paging back to the other folder which doesn't support core.

--- src/ToracGolfMiddleLayer/GridCommon/EFPaging.cs
+++ src/ToracGolfMiddleLayer/GridCommon/EFPaging.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ToracLibrary.AspNet.Paging
+namespace ToracGolf.MiddleLayer.GridCommon
 {
     public static class EFPaging
     {
