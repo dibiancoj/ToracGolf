2b48f7c7c90d5518982d1a587725be67b8d3df25
dibiancoj <dibiancoj@gmail.com>
2015-12-29 14:04:31 -0500

    move ef paging to asp.net common

--- src/ToracLibrary.AspNet/Paging/EFPaging.cs
+++ src/ToracLibrary.AspNet/Paging/EFPaging.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ToracGolf.MiddleLayer.GridCommon
+namespace ToracLibrary.AspNet.Paging
 {
     public static class EFPaging
     {
