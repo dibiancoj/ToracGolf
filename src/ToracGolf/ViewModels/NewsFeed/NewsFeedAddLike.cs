d5dc7f3733585acd7ee7d4dc62122e272db3295c
Jason <dibiancoj@gmail.com>
2016-01-03 18:14:12 -0500

    add missing file

---
+++ src/ToracGolf/ViewModels/NewsFeed/NewsFeedAddLike.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToracGolf.MiddleLayer.NewsFeed;
+
+namespace ToracGolf.ViewModels.NewsFeed
+{
+    public class NewsFeedAddLike
+    {
+        public int Id { get; set; }
+        public NewsFeedItemModel.NewsFeedTypeId NewsFeedTypeId { get; set; }
+    }
+}
