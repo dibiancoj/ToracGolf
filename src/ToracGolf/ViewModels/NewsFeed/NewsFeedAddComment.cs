8fa62b4248347262ee01f7e8a2a5d5373ed35609
Jason <dibiancoj@gmail.com>
2016-01-17 11:46:36 -0500

    add missing cs file from last checkin

---
+++ src/ToracGolf/ViewModels/NewsFeed/NewsFeedAddComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToracGolf.MiddleLayer.NewsFeed;
+
+namespace ToracGolf.ViewModels.NewsFeed
+{
+    public class NewsFeedAddComment : NewsFeedAddLike
+    {
+        public string CommentToAdd { get; set; }
+    }
+}
