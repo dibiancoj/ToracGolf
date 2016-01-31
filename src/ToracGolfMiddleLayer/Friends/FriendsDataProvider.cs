a1412a167af06a4ed0db6d0303099475b7e6f577
Jason <dibiancoj@gmail.com>
2016-01-31 00:11:51 -0500

    add missing file

---
+++ src/ToracGolfMiddleLayer/Friends/FriendsDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToracGolf.MiddleLayer.EFModel;
+
+namespace ToracGolf.MiddleLayer.Friend
+{
+    public static class FriendsDataProvider
+    {
+
+        public static IQueryable<int> GetUsersFriendList(ToracGolfContext dbContext, int userIdToGetFriendsOf)
+        {
+            return dbContext.Friends.AsNoTracking().Where(x => x.UserId == userIdToGetFriendsOf).Select(x => x.FriendId);
+        }
+
+    }
+}
