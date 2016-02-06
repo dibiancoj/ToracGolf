48f2becb54710bab38cf928011ffa00dab6fe056
Jason <dibiancoj@gmail.com>
2016-02-05 21:22:24 -0500

    fix publish for angular 2

--- src/ToracGolf/wwwroot/js/Angular2/NewsFeed/boot.js
+++ src/ToracGolf/wwwroot/js/Angular2/NewsFeed/boot.js
@@ -1,5 +1,5 @@
-System.register(['angular2/platform/browser', './NewsFeedApp', 'angular2/http', '../Common/httpinterceptor'], function(exports_1) {
-    var browser_1, NewsFeedApp_1, http_1, httpinterceptor_1;
+System.register(['angular2/platform/browser', './NewsFeedApp', 'angular2/core', 'angular2/http', '../Common/httpinterceptor'], function(exports_1) {
+    var browser_1, NewsFeedApp_1, core_1, http_1, httpinterceptor_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -8,6 +8,9 @@ System.register(['angular2/platform/browser', './NewsFeedApp', 'angular2/http', 
             function (NewsFeedApp_1_1) {
                 NewsFeedApp_1 = NewsFeedApp_1_1;
             },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
             function (http_1_1) {
                 http_1 = http_1_1;
             },
@@ -15,8 +18,7 @@ System.register(['angular2/platform/browser', './NewsFeedApp', 'angular2/http', 
                 httpinterceptor_1 = httpinterceptor_1_1;
             }],
         execute: function() {
-            //enableProdMode();
-            debugger;
+            core_1.enableProdMode();
             browser_1.bootstrap(NewsFeedApp_1.NewsFeedApp, [http_1.HTTP_PROVIDERS, httpinterceptor_1.HttpInterceptor]);
         }
     }
