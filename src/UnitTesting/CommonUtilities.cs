dc05b811d74ac8285f72ca5818ed018900c38b65
Jason <dibiancoj@gmail.com>
2016-04-02 15:09:58 -0400

    start of xunit unit testing.

---
+++ src/UnitTesting/CommonUtilities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTesting
+{
+
+    public class CommonUtilities
+    {
+
+        // This project can output the Class library as a NuGet Package.
+        // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(4, 4);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+    }
+
+}
---
+++ src/UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45b43a52-bfea-4a32-b476-4d2275550aaf")]
